88a5151be2eb65f90c7c93a109c2bbb04a02f339
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2009-06-17 09:46:56 +0000

    add hltscaler inspection

--- test/inspectLuminosityInfo.py
+++ test/inspectLuminosityInfo.py
@@ -6,12 +6,13 @@ from pluginCondDBPyInterface import *
 a = FWIncantation()
 rdbms = RDBMS()
 dbName =  "sqlite_file:offlinelumi.db"
-
+print 1
 from CondCore.Utilities import iovInspector as inspect
-
+print 3
 db = rdbms.getDB(dbName)
+print 4
 tags = db.allTags()
-
+print 'all tags ',tags
 tag = 'lumitest'
 
 try :
@@ -24,9 +25,9 @@ try :
     print "===payload dump lumisec 1==="
     payloadstrdump=iov.payloadSummaries()
     print payloadstrdump[0]
-    #for p in iovlist:
-    #    payload=inspect.PayLoad(db,p[0])
-    #    print payload.summary()
+    for p in iovlist:
+        payload=inspect.PayLoad(db,p[0])
+        print payload.summary()
     print "===print lumi vs time values==="
     print inspect.extractorWhat(db,tag)
     #print 'trend lumivalue vs. lumiid'
@@ -41,4 +42,21 @@ try :
 except Exception, er :
     print er
 
+print "-------------------------------------------------------"
+tag2 = 'hltscalertest'
 
+try :
+    iov2 = inspect.Iov(db,tag2)
+    print "===iov list ==="
+    iov2list=iov2.list()
+    print iov2list
+    print "===payload 2 dump lumisec 1==="
+    payloadstrdump2=iov2.payloadSummaries()
+    print payloadstrdump2[0]
+    for p in iov2list:
+        payload=inspect.PayLoad(db,p[0])
+        print payload.summary()
+    #print 'trend lumivalue vs. lumiid'
+    print iov2.payloadContainerName()
+except Exception, er :
+    print er
